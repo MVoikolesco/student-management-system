fa6e1dc3b4dd8764125efea53034f1cfe009297e
mvoikolesco <marcioavj.ti@outlook.com>
2025-07-27 23:52:37 -0300

    feat(utils): added wrapper for async controllers with error handling

---
+++ backend/src/utils/controllerWrapper.ts
@@ -0,0 +1,13 @@
+import { Request, Response, NextFunction } from 'express';
+
+type AsyncController = (req: Request, res: Response, next: NextFunction) => Promise<any>;
+
+export const controllerWrapper = (controller: AsyncController) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      await controller(req, res, next);
+    } catch (error) {
+      next(error);
+    }
+  };
+}; 
